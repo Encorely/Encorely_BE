5863796e8352ffcd6dfaaf8099d4f73edd7b6ac2
keypang <bestnest820@naver.com>
2025-08-01 15:21:22 +0900

    feat: API 설명 추가

--- src/main/java/spring/encorely/controller/scrapFileController/ScrapFileController.java
+++ src/main/java/spring/encorely/controller/scrapFileController/ScrapFileController.java
@@ -1,5 +1,6 @@
 package spring.encorely.controller.scrapFileController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,17 +18,20 @@ public class ScrapFileController {
     private final ScrapFileService scrapFileService;
 
     @PostMapping("/{fileId}/review")
+    @Operation(summary = "스크랩 파일에 리뷰 등록")
     public ApiResponse<ScrapFileResponseDTO.addReviewToFile> addReviewToFile(@PathVariable Long fileId,
                                                                              @RequestBody ScrapFileRequestDTO.addReviewToFile request) {
         return ApiResponse.onSuccess(fileService.addReviewToFile(fileId, request));
     }
 
     @PostMapping
+    @Operation(summary = "스크랩 파일 추가")
     public ApiResponse<ScrapFileResponseDTO.addFile> addFile(@AuthenticationPrincipal UserDetails userDetails) {
         return ApiResponse.onSuccess(scrapFileService.addFile(Long.parseLong(userDetails.getUsername())));
     }
 
     @PatchMapping("/{fileId}")
+    @Operation(summary = "스크랩 파일 이름 변경")
     public ApiResponse<ScrapFileResponseDTO.updateFileName> updateFileName(@AuthenticationPrincipal UserDetails userDetails,
                                                                            @PathVariable Long fileId,
                                                                            @RequestBody ScrapFileRequestDTO.updateFileName request) {

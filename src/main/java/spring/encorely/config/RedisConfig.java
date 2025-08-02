c7761ece1ecc8584deefcd81eadd42e40393f196
shlee <kwlee0100@naver.com>
2025-08-02 13:56:30 +0900

    [Refactor] 도커 파일 수정

--- src/main/java/spring/encorely/config/RedisConfig.java
+++ src/main/java/spring/encorely/config/RedisConfig.java
@@ -1,18 +1,28 @@
 package spring.encorely.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
+        return factory;
     }
 
     @Bean
